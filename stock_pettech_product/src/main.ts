bb39c09168f689eaed69cd33c3bb36d6556a5165
Stella <yanostella14@gmail.com>
2025-02-11 16:06:35 -0300

    Add git workflow

--- stock_pettech_product/src/main.ts
+++ stock_pettech_product/src/main.ts
@@ -5,6 +5,6 @@ import { HttpExceptionFilter } from './shared/filters/http-exception.filter';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalFilters(new HttpExceptionFilter());
-  await app.listen(process.env.PORT || 3000);
+  await app.listen(process.env.PORT || 3010);
 }
 bootstrap();
